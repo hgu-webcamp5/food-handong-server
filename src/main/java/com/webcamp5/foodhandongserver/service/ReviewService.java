31b601bf2ccc48fe4f4bdb69d25e0833faa986c1
KimSeongKyung <89564518+KimSeongKyung@users.noreply.github.com>
2023-01-13 18:56:13 +0900

    update review server

--- src/main/java/com/webcamp5/foodhandongserver/service/ReviewService.java
+++ src/main/java/com/webcamp5/foodhandongserver/service/ReviewService.java
@@ -1,7 +1,9 @@
 package com.webcamp5.foodhandongserver.service;
 
 import com.webcamp5.foodhandongserver.model.Review;
+import com.webcamp5.foodhandongserver.model.User;
 import com.webcamp5.foodhandongserver.model.request.ReviewCreationRequest;
+import com.webcamp5.foodhandongserver.model.request.ReviewReadRequest;
 import com.webcamp5.foodhandongserver.repository.ReviewRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,15 +20,26 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final UserService userService;
 
     public List<Review> readReviewsAll() {
         return reviewRepository.findAll();
     }
 
-    public List<Review> readReviewsRes(int restaurantId) {
+    public List<ReviewReadRequest> readReviewsRes(int restaurantId) {
         List<Review> reviews = reviewRepository.findAllByRestaurantId(restaurantId);
+
         if(!reviews.isEmpty()) {
-            return reviews;
+            List<ReviewReadRequest> result = new ArrayList<>();
+            reviews.forEach(review -> {
+                ReviewReadRequest reviewObj = new ReviewReadRequest();
+                BeanUtils.copyProperties(review,reviewObj);
+
+                reviewObj.setUserName(userService.readUser(review.getUserId()).getName());
+                reviewObj.setProfileUrl(userService.readUser(review.getUserId()).getProfileUrl());
+                result.add(reviewObj);
+            });
+            return result;
         }
         throw new EntityNotFoundException("Can't find any review under given restaurantId");
     }
