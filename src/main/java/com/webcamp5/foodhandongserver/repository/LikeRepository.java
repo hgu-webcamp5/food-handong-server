91210cb851ae78721f7bdaed4c2b0a7d5c073b37
ohinhyuk <8156217@naver.com>
2023-01-19 14:58:31 +0900

    Update LikeRepository.java
    findAllByRestaurantId wasn't merged.
    i added again.

--- src/main/java/com/webcamp5/foodhandongserver/repository/LikeRepository.java
+++ src/main/java/com/webcamp5/foodhandongserver/repository/LikeRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     List<Like> findAllByUserId(Long userId);
+    List<Like> findAllByRestaurantId(Long restaurantId);
 }
